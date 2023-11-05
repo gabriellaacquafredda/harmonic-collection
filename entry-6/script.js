128969f8c3c04bb0810ca968c4bf5ca5ce11f0c7
Gabriella Acquafredda <gabriellaacquafredda@gabriellas-mbp.wireless.newschool.edu>
2023-11-05 17:08:01 -0500

    working out kinks
    .js for final-reveal

--- entry-6/script.js
+++ entry-6/script.js
@@ -1,26 +1,34 @@
-// failed scripts...
-// const hoverDiv = document.querySelector('.hov-div');
-
-// hoverDiv.addEventListener('mouseover', function() {
-//     hoverDiv.classList.add('show-text');
-// });
+// gen
+let totalRevealed = 0;
 
 // hover to reveal
 const hoverDivs = document.querySelectorAll('.hov-div , .hov-div2 , .hov-div3 , .hov-div4 , .hov-div5');
 console.log(hoverDivs);
 
+// this is the goal, but needs to happen only after all have revealed themselves
+// this needs to be a function that you can then call from within the mouseevent
+
 hoverDivs.forEach(function(hoverDiv) {
-    hoverDiv.addEventListener('mouseover', function() {
-        hoverDiv.classList.add('show-text');
-    });
+  let hasRevealed = false;  
+
+  hoverDiv.addEventListener('mouseenter', function() {
+    hoverDiv.classList.add('show-text');
+    
+    if (hasRevealed === false) {
+      totalRevealed++;
+      hasRevealed = true;
+    }
+    if (totalRevealed === 5) {
+      console.log("yuh")
+      let finalReveal = document.getElementById('final-reveal');
+      finalReveal.style.setProperty('display', 'flex');
+    }
+    console.log(totalRevealed)
+  });
+
+
 });
 
-// moving on
-const triggerButton = document.getElementsByClassName('.hov-div', 'hov-div2');
-const animatedElement = document.getElementById('moving-on');
 
-function startAnimation() {
-  animatedElement.classList.add('animated');
-}
 
 
